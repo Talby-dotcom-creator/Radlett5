561dfcef6b387c6ff0208e45cfaf270af18c5a6d
Talby-dotcom-creator <Ptalbot37@gmail.com>
2025-08-03 16:12:11 +0100

    Fix: Recreate and restore HomePage.tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Radlett Lodge</h1>
+      <p>This is the homepage. More great content coming soon!</p>
+    </div>
+  );
+};
+
+export default HomePage;
